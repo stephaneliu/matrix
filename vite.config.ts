bf65641a7931b493c46a4fa23cbdc9edeb13070a
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-05-22 22:35:35 -1000

    Chore: Add vite plugins
      - yarn add stimulus-vite-helpers vite-plugin-stimulus-hmr vite-plugin-full-reload
      - stimulus-vite-helpers
        - Easily register all Stimulus controlers from globEager
        - https://vite-ruby.netlify.app/guide/plugins.html#stimulus-helpers
      - stimulus-hmr
        - See changers to stimulus controllers instantly without page refresh.
        - https://vite-ruby.netlify.app/guide/plugins.html#stimulus-hmr
      - full-reload
        - Pager reload helper when making changes to server-rendered layouts and templates
        - https://vite-ruby.netlify.app/guide/plugins.html#full-reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import FullReload from 'vite-plugin-full-reload'
+import StimulusHMR from 'vite-plugin-stimulus-hmr'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
   plugins: [
+    FullReload(['config/routes.rb', 'app/views/**/*']),
+    StimulusHMR(),
     RubyPlugin(),
   ],
 })
